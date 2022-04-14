a10adecfa2668baff532be2b81315a8a46d1704d
Arthur <arthur@corenzan.com>
2022-04-14 19:50:02 -0300

    Use docker-compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,5 @@ if test -n "$1"; then
 	exit 0
 fi
 
-./valheim_server.x86_64 \
-	-name     "$VALHEIM_SERVER_NAME" \
-	-world    "$VALHEIM_SERVER_WORLD" \
-	-password "$VALHEIM_SERVER_PASSWORD"
+./valheim_server.x86_64 -name "$VALHEIM_SERVER_NAME" \
+	-world "$VALHEIM_SERVER_WORLD" -password "$VALHEIM_SERVER_PASSWORD"
