d2f736dfa57ef6f8123f30102e59eac4985b1e4a
Arthur <arthur@corenzan.com>
2024-10-25 22:10:46 -0300

    Fix wrong path

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ fi
 export LD_LIBRARY_PATH="./app/linux64:$LD_LIBRARY_PATH"
 export SteamAppId="892970"
 
-./valheim_server.x86_64 -nographics -batchmode \
+./app/valheim_server.x86_64 -nographics -batchmode \
 	-name "$VALHEIM_SERVER_NAME" \
 	-world "$VALHEIM_SERVER_WORLD" \
 	-password "$VALHEIM_SERVER_PASSWORD"
